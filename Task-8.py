7a8cf96ef2af311b6b3c9eb44677c8b8cc12cc0e
jagan <43448819+jagannath-swarnkar@users.noreply.github.com>
2019-05-09 19:21:15 +0530

    HttpRequestNotAllowed

---
+++ Task-8.py
@@ -0,0 +1,21 @@
+# impporting modules requered for the programe
+from flask import Flask, jsonify, request,abort
+app = Flask(__name__)
+
+# creating route and defining methods can used for this endpoint
+@app.route('/', methods=['GET','POST'])
+def hello_world():
+	method = request.method
+	# if method == 'GET':
+	# 	abort(497)
+	# else:
+	# 	name = request.json.get('name')
+	# 	return("name = "+name)
+	if method == 'POST':
+		name = request.json.get('name')
+		return ("Hello, {}".format(name))
+	else:
+		abort(405)
+
+#listening with port and host with debuging
+app.run(host = '127.0.0.1', port='8080', debug=True)
